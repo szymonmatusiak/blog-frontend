65932aeb292d7ce7e0dfadb911530c09388a4827
Szymon Matusiak <218344@student.pwr.wroc.pl>
2018-05-01 22:04:06 +0200

    bulma .

---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="Login">
+
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h2>Ecosystem</h2>
+    <h2 v-bind="msg"></h2>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Test",
+
+      data() {
+        return {
+          msg: 'Welcome to Your Vue.js App'
+        }
+      }
+    }
+</script>
+
+<style>
+
+</style>
