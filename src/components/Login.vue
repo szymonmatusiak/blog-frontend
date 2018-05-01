6fd5a65be507edf9ba76ca29cbc76c108d27a2c3
Szymon Matusiak <218344@student.pwr.wroc.pl>
2018-05-02 01:43:24 +0200

    login + navBar.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,40 @@
 <template>
-  <div class="Login">
+
+  <div class="hello" >
+    <div class="container" >
+      <div class="field">
+        <p class="control has-icons-left ">
+          <input class="input" type="email" placeholder="Email">
+          <span class="icon is-small is-left">
+           <i class="fas fa-envelope"></i>
+          </span>
+        </p>
+      </div>
+      <div class="field">
+        <p class="control has-icons-left">
+          <input class="input" type="password" placeholder="Password">
+          <span class="icon is-small is-left">
+           <i class="fas fa-lock"></i>
+          </span>
+        </p>
+      </div>
+      <a class="button is-primary" >Login</a>
+
+    </div>
+  </div>
 
 </template>
 
 <script>
-    export default {
-        name: "Login"
-    }
+export default {
+    name: "Login"
+}
 </script>
 
 <style scoped>
-
+  .field{
+    margin: auto;
+    max-width: 420px;
+    margin-bottom: 10px;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Test from '@/components/Test'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
 
   routes: [
     {
@@ -13,9 +15,13 @@ export default new Router({
       name: 'HelloWorld',
       component: HelloWorld
     },{
-      path: '/login',
+      path: '/test',
       name: 'Test',
       component: Test
+    },{
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
